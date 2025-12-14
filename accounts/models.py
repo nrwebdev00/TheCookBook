53e53bcd74a4259a6e128e39072f592a4ebd3d2c
Nathon Reed <nrwebdev00@gmail.com>
2025-12-14 09:24:29 -0800

    first commit setup project and create a custom user using email instead of username and remove first and last name and repleaced it with name, created a core app that will hold core abilities and dev routes for this project.

---
+++ accounts/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, name, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"   # login with email
+    REQUIRED_FIELDS = ["name"] # prompted when creating superuser
+
+    def __str__(self):
+        return self.email
